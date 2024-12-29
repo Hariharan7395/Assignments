d8b983ffb23f4c5dd91632fe0aa8643bc6fac653
HARIHARAN S <hariharan.s@yantravision.com>
2024-12-29 22:03:34 +0530

    Update main.c

--- C_assignment/main.c
+++ C_assignment/main.c
@@ -17,28 +17,28 @@
 // Function to display the menu
 void display_menu();
 
-int main() {
+int main(){
         initialise_trains(); // Initialize train data
-        while (1){
+        while(1){
                 int option_selected;
                 display_menu();
                 printf("Enter your choice: ");
                 scanf("%d",&option_selected);
                 switch (option_selected){
                         case Booking_option:
-                                search_train(); // Search and book a train
+                                search_train(); //Search and book a train
                                 break;
                         case Cancellation_option:
-                                cancel_ticket(); // Cancel a ticket
+                                cancel_ticket();// Cancel a ticket
                                 break;
                         case Exit_option:
                                 printf("Thank you for using the Booking System.\n");
-                                exit(0); // Exit the program
+                                exit(0);// Exit the program
                         default:
                                 printf("Error: Invalid option.\n");
                 }
         }
-        return 0; // Program should never reach this point
+        return 0; //Program should never reach this point
 }
 
 //Function to display menu
